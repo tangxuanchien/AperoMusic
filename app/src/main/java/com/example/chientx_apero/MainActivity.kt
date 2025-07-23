8bb105b1e257ef844ddc691a98ab3cc04dc59bd3
chientx-apero <chientx@talent.apero.vn>
2025-07-23 10:18:32 +0700

    Test connect PR

--- app/src/main/java/com/example/chientx_apero/MainActivity.kt
+++ app/src/main/java/com/example/chientx_apero/MainActivity.kt
@@ -4,11 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.chientx_apero.ui.theme.ChientxAperoTheme
@@ -32,6 +38,14 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Column {
+        var name by remember{ mutableStateOf("") }
+        OutlinedTextField(
+            value = name,
+            onValueChange = { name = it },
+            label = { Text("Name: $name")}
+        )
+    }
     Text(
         text = "Hello $name!",
         modifier = modifier
